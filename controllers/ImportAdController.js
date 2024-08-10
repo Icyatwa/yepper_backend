7cff48f6d850c04a75669c687efb8fa5ea517cc8
unknown <icyatwandoba@gmail.com>
2024-08-10 12:05:51 -0700

    backend

--- controllers/ImportAdController.js
+++ controllers/ImportAdController.js
@@ -86,12 +86,12 @@ exports.getAllAds = async (req, res) => {
 
 exports.getAdsByUserId = async (req, res) => {
   const userId = req.params.userId;
-  try { 
-    const ad = await ImportAd.find({ userId });
-    if (!ad) {
-      return res.status(404).json({ message: 'Ad not found' });
+  try {
+    const ads = await ImportAd.find({ userId });
+    if (!ads || ads.length === 0) {
+      return res.status(404).json({ message: 'No ads found for this user' });
     }
-    res.status(200).json(ad);
+    res.status(200).json(ads); // Send the ads array directly
   } catch (error) {
     console.error('Error fetching ad by user ID:', error);
     res.status(500).json({ message: 'Internal Server Error' });
